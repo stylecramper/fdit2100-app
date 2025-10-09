9e1e77817c81b27dda0de0d7153c2d844469d3cd
Matt Anderson <him@backattheranch.ca>
2025-10-09 10:29:33 -0700

    filter members list using search field

--- src/components/ui/members/MembersList.tsx
+++ src/components/ui/members/MembersList.tsx
@@ -1,11 +1,20 @@
+import { useCallback, useState } from 'react';
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { Input } from '@/components/ui/input';
 import Member from './Member';
 import { fetchMembers } from "@/lib/api";
+import { type Member as MemberType } from '@/lib/types/member';
 import styles from  './membersList.module.css';
 
 export default function MembersList() {
+    const [nameFilter, setNameFilter] = useState('');
+
+    const filterMembers = useCallback((member: MemberType) => {
+        return member.firstName.toLowerCase().includes(nameFilter.toLowerCase()) ||
+            member.lastName.toLowerCase().includes(nameFilter.toLowerCase());
+    }, [nameFilter]);
+
     const { data } = useSuspenseQuery({
         queryKey: ['members'],
         queryFn: () => fetchMembers(),
@@ -14,10 +23,12 @@ export default function MembersList() {
 
     return (
         <>
-            <Input name="search-members" placeholder="Search members" className={styles.searchField} />
+            <Input name="search-members" value={nameFilter} onChange={event => setNameFilter(event.target.value)} placeholder="Search members" className={styles.searchField} />
             <MagnifyingGlassIcon className="size-5 -translate-y-8 translate-3 text-gray-400" />
             <ul className="mt-3">
-                {data.users.map(user => (
+                {data.users
+                .filter(filterMembers)
+                .map(user => (
                     <Member key={user.id} member={user} />
                 ))}
             </ul>
