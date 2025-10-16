cfb632b324562cb8ff4e9cee8ae645954a82b463
Matt Anderson <him@backattheranch.ca>
2025-10-16 11:31:23 -0700

    x icon to clear search field

--- src/components/ui/members/MembersList.tsx
+++ src/components/ui/members/MembersList.tsx
@@ -1,6 +1,7 @@
-import { useMemo, useState } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { XMarkIcon } from '@heroicons/react/24/solid';
 import { Input } from '@/components/ui/input';
 import Member from './Member';
 import { fetchMembers } from "@/lib/api";
@@ -9,6 +10,7 @@ import styles from  './membersList.module.css';
 
 export default function MembersList() {
     const [nameFilter, setNameFilter] = useState('');
+    const inputRef = useRef<HTMLInputElement>(null);
 
     /* const filterMembers = useCallback((member: MemberType) => {
         return member.firstName.toLowerCase().includes(nameFilter.toLowerCase()) ||
@@ -26,10 +28,16 @@ export default function MembersList() {
             member.lastName.toLowerCase().includes(nameFilter.toLowerCase())
     ), [data.users, nameFilter]);
 
+    const clearField = useCallback(() => {
+        setNameFilter('');
+        inputRef.current?.focus();
+    }, []);
+
     return (
         <>
-            <Input name="search-members" value={nameFilter} onChange={event => setNameFilter(event.target.value)} placeholder="Search members" className={styles.searchField} />
-            <MagnifyingGlassIcon className="size-5 -translate-y-8 translate-3 text-gray-400" />
+            <Input name="search-members" value={nameFilter} onChange={event => setNameFilter(event.target.value)} placeholder="Search members" className={styles.searchField} ref={inputRef} />
+            <MagnifyingGlassIcon className="size-5 relative -top-8 left-4 text-gray-400" />
+            <XMarkIcon className={`ml-auto -translate-y-13 -translate-6 size-5 text-gray-400 cursor-pointer ${nameFilter ? 'visible' : 'invisible'}`} onClick={clearField} />
             <ul className="mt-3">
                 {filteredMembers.length === 0 ? (
                     <li className="text-gray-500">No members found. Try a different search?</li>
