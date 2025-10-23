f3d3cda554e09daf9f5407898664ea11699c5c46
Matt Anderson <him@backattheranch.ca>
2025-10-23 10:41:49 -0700

    return user to previous after login

--- src/screens/login/Login.tsx
+++ src/screens/login/Login.tsx
@@ -1,4 +1,5 @@
 import { useCallback } from "react";
+import { useNavigate } from 'react-router';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
@@ -28,11 +29,16 @@ export default function Login() {
         }
     });
 
+    const navigate = useNavigate();
     const { login } = useAppStore();
 
     const onLogin = useCallback(async (values: z.infer<typeof loginSchema>) => {
-        await login(values);
-    }, [login]);
+        const loginResult = await login(values);
+        if (loginResult?.accessToken) {
+            navigate(-1);
+            return null;
+        }
+    }, [login, navigate]);
 
     return (
         <div className="flex flex-col items-center justify-center">
